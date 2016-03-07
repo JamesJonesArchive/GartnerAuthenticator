8fccad86b0d129b6a24a34cddff99b1be60581cb
James Jones <jamjon3@gmail.com>
2016-03-07 11:22:50 -0500

    Getting the controller all set

--- app/src/Action/GartnerAction.php
+++ app/src/Action/GartnerAction.php
@@ -16,54 +16,44 @@ class GartnerAction extends \USF\IdM\AuthTransfer\BasicAuthServiceAction {
     
     public function dispatch(\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response, array $args) {
         $view_attrs = [];
-        $netid = $request->getHeaderLine('AUTH_PRINCIPAL');
-        $ipAddress = $request->getAttribute('ip_address');
-        $application = $args['application'] ?? 'UNKNOWN';
-        $instance = $args['instance'] ?? $request->getQueryParams()['instance'] ?? $this->settings['defaultInstance'] ?? 'production';
-        $deepLink = $request->getQueryParams()['uri'] ?? '';
-        $selectUsername = $request->getQueryParams()['accountSelection'] ?? '';
-        // Ensure the passed instance is configured
-        if (!isset($this->settings['instances'][$instance])) {
-            $view_attrs['statusText'] = 'Configuration Error';
-            $view_attrs['message'] = "The Gartner instance <em><strong>${instance}</strong></em> is not configured in this application.";
-            $this->logger->error("${application}|NO_INSTANCE_FOUND|${instance}|${ipAddress}|${netid}");
-            return $this->view->render($response, 'error.html', $view_attrs);
+        //Grab the Resource ID and/or the Document Code from the GET string
+        $resId = $args["resId"] ?? '';
+        $docCode = $args["docCode"] ?? '';
+
+        //Netid will be sent to Gartner as 'uid'
+        $username = $request->getHeaderLine('AUTH_PRINCIPAL');
+
+        //eduPersonPrimaryAffiliation will be used for authorization and sent to Gartner as 'title'
+        $eppa = $request->getHeaderLine('AUTH_ATTR_EDUPERSONPRIMARYAFFILIATION');
+        
+        $allowedEppaList = $this->authService->settings['gartner']['allowedEppaList'] ?? [];
+        if(\in_array($eppa, $allowedEppaList)) {
+            $this->logger->error("${application}|Gartner access denied for ePPA|${eppa}|${username}");            
+            // Redirect to an error page if the EPPA isn't allowed
+            return $this->view->render($response, 'error.html', ['allowedEppaList' => $allowedEppaList ]);
         }
-        // Ensure the passed application is configured
-        if (!isset($this->settings['instances'][$instance][$application])) {
-            $view_attrs['statusText'] = 'Configuration Error';
-            $view_attrs['message'] = "The Gartner application <em><strong>${application}</strong></em> is not configured for authentication.";
-            $this->logger->error("${application}|NO_APPLICATION_FOUND|${instance}|${ipAddress}|${netid}");
-            return $this->view->render($response, 'error.html', $view_attrs);
-        }
-        try {
-            $result = $this->authService->getRedirectUrl($application, $instance, $netid, $ipAddress, $deepLink, $selectUsername);
-            // The result was a list of usernames
-            if (is_array($result)) {
-                $view_attrs['account_list'] = $result;
-                $view_attrs['application']  = $application;
-                $view_attrs['instance']     = $instance;
-                $view_attrs['deepLink']     = $deepLink;
-                return $this->view->render($response, 'select_account.html', $view_attrs);
-            }
-            // The result was a URL for the Gartner Application
-            $this->logger->info("${application}|REDIRECT|${instance}|${ipAddress}|${netid}|${result}");
-            return $response->withRedirect($result);
-        } catch (NoAccountFoundException $e) {
-            $view_attrs['message'] = $e->getMessage();
-            $this->logger->warn("${application}|NO_ACCOUNT_FOUND|${instance}|${ipAddress}|${netid}");
-            $this->view->render($response, 'account_error.html', $view_attrs);
-        } catch (MultipleAccountsFoundException $e) {
-            $view_attrs['message'] = $e->getMessage();
-            $this->logger->warn("${application}|MULTIPLE_ACCOUNTS_FOUND|${instance}|${ipAddress}|${netid}");
-            $this->view->render($response, 'account_error.html', $view_attrs);
-        } catch (\Exception $e) {
-            $view_attrs['statusText'] = 'Authentication Error';
-            $view_attrs['message'] = $e->getMessage();
-            $view_attrs['file'] = $e->getFile() . ':' . $e->getLine();
-            $this->logger->warn("${application}|GENERIC_ERROR|${instance}|${ipAddress}|${netid}|${view_attrs['message']}");
-            $this->view->render($response, 'error.html', $view_attrs);
-        }
+        
+        //usfEduCampus is the USF Campus
+        $campus = $request->getHeaderLine('AUTH_ATTR_USFEDUCAMPUS') ?? '';
+        //GivenName is the first name
+        $firstName = $request->getHeaderLine('AUTH_ATTR_GIVENNAME') ?? '';        
+        //Surname is the last name
+        $lastName = $request->getHeaderLine('AUTH_ATTR_SURNAME') ?? '';
+        // Mail is  the email address
+        $email = $request->getHeaderLine('AUTH_ATTR_MAIL') ?? '';
+        $gartnerURL = $this->authService->getRedirectUrl([
+            'uid' => $username,
+            'fn' => $firstName,
+            'ln' => $lastName,
+            'em' => $email,
+            'title' => $eppa,
+            'other' => $campus,
+            'resId' => $resId,
+            'docCode' => $docCode 
+        ]);
+        // The result was a URL for the Gartner Application
+        $this->logger->info("${application}|REDIRECT|${username}|${result}");
+        return $response->withRedirect($result);
     }
 
 }
